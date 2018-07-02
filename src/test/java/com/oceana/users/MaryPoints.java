013c54bcf48c656627cf3ae9928851f6684160b6
Ocean <dvsocean@icloud.com>
2018-07-01 19:40:28 -0700

    veri

--- src/test/java/com/oceana/users/MaryPoints.java
+++ src/test/java/com/oceana/users/MaryPoints.java
@@ -2,10 +2,10 @@ package com.oceana.users;
 
 public class MaryPoints extends User{
 
-  private String aadvn_number = "5X78V88";
-  private String last_name = "Points";
-  private String first_name = "Mary";
-  private String password = "testing";
+  private String aadvn_number = "";
+  private String last_name = "";
+  private String first_name = "";
+  private String password = "";
 
   public String getAadvn_number() {
     return aadvn_number;
