a63a4da395bd362bb27bf0c0eb8588237eb1a3f3
Ocean <dvsocean@icloud.com>
2018-07-01 19:42:50 -0700

    removed

--- src/test/java/com/oceana/interfaces/HomePageActions.java
+++
@@ -1,9 +0,0 @@
-package com.oceana.interfaces;
-
-import com.oceana.pages.HomePage;
-
-public interface HomePageActions {
-
-  HomePage login(Object user);
-
-}
--- src/test/java/com/oceana/users/User.java
+++
@@ -1,3 +0,0 @@
-package com.oceana.users;
-
-public class User {}
