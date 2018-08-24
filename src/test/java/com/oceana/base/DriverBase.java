a975f0b83df54dea5db786cf73bcb4bc11f93723
Ocean <dvsocean@icloud.com>
2018-08-24 06:48:06 -0700

    remote driver dependencies added

--- src/test/java/com/oceana/base/DriverBase.java
+++ src/test/java/com/oceana/base/DriverBase.java
@@ -4,8 +4,10 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.oceana.managers.User;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -32,7 +34,11 @@ public class DriverBase extends InitDriverBase {
   }
 
   @AfterMethod
-  public void tearDown(){
+  public void tearDown(ITestResult result){
+    String os = System.getProperty("os.name").toLowerCase();
+    if (os.contains("linux")) {
+      ((JavascriptExecutor) WebDriverRunner.getWebDriver()).executeScript("sauce:job-result=" + (result.isSuccess() ? "passed" : "failed"));
+    }
     Selenide.close();
   }
 }
--- src/test/java/com/oceana/managers/ObjectManager.java
+++ src/test/java/com/oceana/managers/ObjectManager.java
@@ -5,6 +5,9 @@ import static com.codeborne.selenide.Selenide.open;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.oceana.pages.HomePage;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.ios.IOSDriver;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,6 +21,13 @@ public class ObjectManager {
   public ObjectManager() {
     homePage = new HomePage();
   }
+
+  /*
+      System.setProperty("http.proxyHost", "proxy.name.name.com");
+      System.setProperty("http.proxyPort", "2525");
+      System.setProperty("https.proxyHost", "proxy.name.name.com");
+      System.setProperty("https.proxyPort", "2525");
+   */
 
   public HomePage onTheDesktopHomePageInChrome(Method method, String url) throws MalformedURLException {
     DesiredCapabilities caps = DesiredCapabilities.chrome();
@@ -44,5 +54,35 @@ public class ObjectManager {
     Configuration.timeout = 10000;
     open(url);
     return homePage;
+  }
+
+  public void onTheAndroidApp() throws MalformedURLException {
+    // If using a local file --> File file = new File("app-1.apk");
+    DesiredCapabilities cap = new DesiredCapabilities();
+    //cap.setCapability("name", NativeMobileBase.class.getSimpleName() + " > See report for breakdown");
+    cap.setCapability("appiumVersion", "1.6.4");
+    cap.setCapability("deviceName", "Android Emulator");
+    cap.setCapability("deviceOrientation", "portrait");
+    cap.setCapability("browserName", "");
+    cap.setCapability("platformVersion", "6.0");
+    cap.setCapability("platformName", "Android");
+    cap.setCapability("app", "sauce-storage:app-1.apk");
+    //AndroidDriver driver = new AndroidDriver();
+    AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://AccountName:468r345w2-h765-32y3-n4f4-324356q3456t@ondemand.saucelabs.com:443/wd/hub"), cap);
+    WebDriverRunner.setWebDriver(driver);
+  }
+
+  public void onTheIOSApp() throws MalformedURLException {
+    DesiredCapabilities caps = DesiredCapabilities.iphone();
+    //caps.setCapability("name", NativeMobileBase.class.getSimpleName() + " > See report for breakdown");
+    caps.setCapability("appiumVersion", "1.6.4");
+    caps.setCapability("deviceName", "iPhone 7");
+    caps.setCapability("deviceOrientation", "portrait");
+    caps.setCapability("platformVersion", "10.3");
+    caps.setCapability("platformName", "iOS");
+    caps.setCapability("browserName", "Safari");
+    caps.setCapability("app", "sauce-storage:myIOSapp.app");
+    IOSDriver driver = new IOSDriver<>(new URL("http://AccountName:468r345w2-h765-32y3-n4f4-324356q3456t@ondemand.saucelabs.com:443/wd/hub"), caps);
+    WebDriverRunner.setWebDriver(driver);
   }
 }
